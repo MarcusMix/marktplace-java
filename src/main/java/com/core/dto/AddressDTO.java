3e1cedd01744bf41d2b0d1a608c477c5a2918d2d
MarcusMix <tradedomarcus@gmail.com>
2024-09-25 20:10:50 -0300

    feat: adress dto

---
+++ src/main/java/com/core/dto/AddressDTO.java
@@ -0,0 +1,17 @@
+package com.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressDTO {
+    private Long id;
+    private String street;
+    private String number;
+    private String neighborhood;
+    private String city;
+    private String state;
+}
