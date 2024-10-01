dc9c63d55a5d457bc582d77f49c0016156a9fe6d
MarcusMix <tradedomarcus@gmail.com>
2024-10-01 11:27:45 -0300

    feat/fix: mapper to dto and to entity

---
+++ src/main/java/com/core/util/ServiceProviderMapper.java
@@ -0,0 +1,28 @@
+package com.core.util;
+
+import com.core.dto.ServiceProviderDTO;
+import com.core.entity.ServiceProvider;
+
+public abstract class ServiceProviderMapper {
+    
+    public static ServiceProviderDTO toServiceProviderDTO(ServiceProvider serviceProvider) {
+
+        return new ServiceProviderDTO(
+            serviceProvider.getId(),
+            serviceProvider.getName(),
+            serviceProvider.getDescription(),
+            serviceProvider.getExperience(),
+            serviceProvider.getImage()
+        );
+    }
+
+    public static ServiceProvider toServiceProvider(ServiceProviderDTO serviceProviderDTO) {
+        return new ServiceProvider(
+            serviceProviderDTO.getId(),
+            serviceProviderDTO.getName(),
+            serviceProviderDTO.getDescription(),
+            serviceProviderDTO.getExperience(),
+            serviceProviderDTO.getImage()
+        );
+    }
+}
