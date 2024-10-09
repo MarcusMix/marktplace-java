26fe923db33772ef2e6d2f62122cd28c64208177
MarcusMix <tradedomarcus@gmail.com>
2024-10-09 20:11:06 -0300

    feat: update/delete/get by id/get all methods (service/controller)

--- src/main/java/com/core/controller/AddressController.java
+++ src/main/java/com/core/controller/AddressController.java
@@ -1,9 +1,16 @@
 package com.core.controller;
 
+import java.util.List;
+import java.util.spi.LocaleNameProvider;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +30,33 @@ public class AddressController {
         AddressDTO addressDTOSalvo = AddressService.save(addressDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(addressDTOSalvo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddressDTO>> listAll() {
+
+        List<AddressDTO> address_list = AddressService.findAll();
+
+        return ResponseEntity.ok(address_list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressDTO> getById(@PathVariable Long id) {
+        AddressDTO address = AddressService.findById(id);
+
+        return ResponseEntity.ok(address);
+    }
+
+    @PutMapping
+    public ResponseEntity<AddressDTO> update(@RequestBody AddressDTO addressDTO) {
+        AddressDTO updatedAddress = AddressService.update(addressDTO);
+        return ResponseEntity.ok(updatedAddress);
+    }
+
+    // Método de deleção (DELETE)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        AddressService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
