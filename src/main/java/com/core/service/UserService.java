6cca093165a98b9626c7cdcb74ecaa1e18c661ce
MarcusMix <tradedomarcus@gmail.com>
2024-10-09 20:37:22 -0300

    feat: update/delete/get by id user

--- src/main/java/com/core/service/UserService.java
+++ src/main/java/com/core/service/UserService.java
@@ -2,8 +2,10 @@ package com.core.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.core.dto.UserDTO;
@@ -11,6 +13,8 @@ import com.core.entity.User;
 import com.core.repository.UserRepository;
 import com.core.util.InputValidation;
 import com.core.util.UserMapper;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -57,6 +61,50 @@ public class UserService {
         }
 
         return usersDTO;
+    }
+
+    public UserDTO findById(Long id) {
+        Optional<User> userOptional = repository.findById(id);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            return UserMapper.toUserDTO(user);
+        } else {
+            throw new EntityNotFoundException("Usuário com ID " + id + " não encontrado!");
+        }
+    }
+
+    // quebrado pq da erro de constraint
+    public void deleteById(Long id) {
+        Optional<User> userOptional = repository.findById(id);
+
+        if (userOptional.isPresent()) {
+            repository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Usuário com ID " + id + " não encontrado.");
+        }
+    }
+
+    public UserDTO update(UserDTO userDTO) {
+        Optional<User> userOptional = repository.findById(userDTO.getId());
+
+        if (!userOptional.isPresent()) {
+            throw new EntityNotFoundException("Usuário com ID " + userDTO.getId() + " não encontrado");
+        }
+
+        User userSalvo = userOptional.get();
+
+        // Mapeando o DTO para a entidade User
+        User user = UserMapper.toUser(userDTO);
+
+        // Copiando as propriedades do objeto User atualizado, exceto o "id"
+        BeanUtils.copyProperties(user, userSalvo, "id");
+
+        // Salvando o usuário atualizado no banco
+        userSalvo = repository.save(userSalvo);
+
+        // Retornando o DTO do usuário atualizado
+        return UserMapper.toUserDTO(userSalvo);
     }
 
 }
