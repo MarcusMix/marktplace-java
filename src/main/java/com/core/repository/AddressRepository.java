3899f35b01f34a50ac30296434fd14ddfa457aa5
MarcusMix <tradedomarcus@gmail.com>
2024-10-16 19:26:06 -0300

    feat: add repository notation

--- src/main/java/com/core/repository/AddressRepository.java
+++ src/main/java/com/core/repository/AddressRepository.java
@@ -1,9 +1,11 @@
 package com.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.core.entity.Address;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     
--- src/main/java/com/core/repository/ServiceProviderRepository.java
+++ src/main/java/com/core/repository/ServiceProviderRepository.java
@@ -1,8 +1,10 @@
 package com.core.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.core.entity.ServiceProvider;
 
+@Repository
 public interface ServiceProviderRepository extends JpaRepository<ServiceProvider, Long> {
 }
