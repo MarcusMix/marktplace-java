8aa8363afa8f2a33ea331079ace6fe9f9a68f805
MarcusMix <tradedomarcus@gmail.com>
2024-11-07 20:12:09 -0300

    feat: implements token jwt to validate user login

---
+++ src/main/java/com/core/service/TokenService.java
@@ -0,0 +1,55 @@
+package com.core.service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.core.entity.User;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                .withIssuer("marktplace")
+                .withSubject(user.getEmail())
+                .withExpiresAt(getExpiration())
+                .sign(algorithm);
+
+            return token;
+        } catch (JWTDecodeException exception) {
+            throw new RuntimeException("Error JWT: ", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                .withIssuer("marktplace")
+                .build()
+                .verify(token)
+                .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant getExpiration() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+    
+}
