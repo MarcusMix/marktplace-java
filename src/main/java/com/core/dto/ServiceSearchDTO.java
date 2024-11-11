209851157690564494135b81d331fd7d9ad7e9cd
Wesley Dutra <Wesley-Dutra17@hotmail.com>
2024-11-10 22:57:30 -0300

    feat: Added search functionality and fixed address DTO

---
+++ src/main/java/com/core/dto/ServiceSearchDTO.java
@@ -0,0 +1,14 @@
+package com.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceSearchDTO {
+
+    private String serviceName;
+    private String userLocation;
+}
--- src/main/java/com/core/repository/ServiceOrderRepository.java
+++ src/main/java/com/core/repository/ServiceOrderRepository.java
@@ -1,10 +1,14 @@
 package com.core.repository;
 
 import com.core.entity.ServiceOrder;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Long> {
-    
+
+    List<ServiceOrder> findByOfferedServiceIdInOrderByRatingDesc(List<Long> offeredServiceIds);
 }
