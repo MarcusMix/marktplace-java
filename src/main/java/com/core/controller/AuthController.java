2b293143c7d9922dc1d5640ec2625ac4a8c4a4c9
MarcusMix <tradedomarcus@gmail.com>
2024-11-16 09:30:22 -0300

    feat: add cross origin *

--- src/main/java/com/core/controller/AuthController.java
+++ src/main/java/com/core/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import com.core.service.TokenService;
 
 @RestController
 @RequestMapping("auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
