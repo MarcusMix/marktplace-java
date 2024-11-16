7677548a248e430f220976e552b43fd5d8ed0403
MarcusMix <tradedomarcus@gmail.com>
2024-11-16 09:31:00 -0300

    feat: add throw error for jwt verification

--- src/main/java/com/core/service/TokenService.java
+++ src/main/java/com/core/service/TokenService.java
@@ -44,7 +44,7 @@ public class TokenService {
                 .verify(token)
                 .getSubject();
         } catch (JWTVerificationException exception) {
-            return "";
+            throw new JWTVerificationException("Token inválido ou expirado.");
         }
     }
 
